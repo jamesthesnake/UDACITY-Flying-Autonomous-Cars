203ea1e3ad36dcd6dd294c1fc93ab56c9cd882e3
James Hennessy <james.ryan.hennessy@gmail.com>
2021-04-11 16:46:06 -0700

    Update planning_utils.py

--- planning_utils.py
+++ planning_utils.py
@@ -1,8 +1,41 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@jamesthesnake 
+allthatido
+/
+Drone_Motion_Planning
+2
+14
+6
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Drone_Motion_Planning/planning_utils.py /
+@allthatido
+allthatido project update V1
+Latest commit f62ed0b on Apr 18, 2018
+ History
+ 1 contributor
+ 182 lines (148 sloc)  5.65 KB
+ 
 from enum import Enum
 from queue import PriorityQueue
 import numpy as np
-import re
-from math import sqrt
+import utm as u
+import matplotlib.pyplot as plt
+from bresenham import bresenham
+
 
 def create_grid(data, drone_altitude, safety_distance):
     """
@@ -21,8 +54,8 @@ def create_grid(data, drone_altitude, safety_distance):
 
     # given the minimum and maximum coordinates we can
     # calculate the size of the grid.
-    north_size = int(np.ceil((north_max - north_min + 1)))
-    east_size = int(np.ceil((east_max - east_min + 1)))
+    north_size = int(np.ceil(north_max - north_min))
+    east_size = int(np.ceil(east_max - east_min))
 
     # Initialize an empty grid
     grid = np.zeros((north_size, east_size))
@@ -32,8 +65,10 @@ def create_grid(data, drone_altitude, safety_distance):
         north, east, alt, d_north, d_east, d_alt = data[i, :]
         if alt + d_alt + safety_distance > drone_altitude:
             obstacle = [
-                int(np.clip(north - d_north - safety_distance - north_min, 0, north_size-1)),
-                int(np.clip(north + d_north + safety_distance - north_min, 0, north_size-1)),
+                int(np.clip(north - d_north - safety_distance -
+                            north_min, 0, north_size-1)),
+                int(np.clip(north + d_north + safety_distance -
+                            north_min, 0, north_size-1)),
                 int(np.clip(east - d_east - safety_distance - east_min, 0, east_size-1)),
                 int(np.clip(east + d_east + safety_distance - east_min, 0, east_size-1)),
             ]
@@ -46,7 +81,6 @@ def create_grid(data, drone_altitude, safety_distance):
 class Action(Enum):
     """
     An action is represented by a 3 element tuple.
-
     The first 2 values are the delta of the action relative
     to the current grid position. The third and final value
     is the cost of performing the action.
@@ -56,10 +90,10 @@ class Action(Enum):
     EAST = (0, 1, 1)
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
-    SOUTH_EAST = (1, 1, sqrt(2))
-    NORTH_EAST = (-1, 1, sqrt(2))
-    SOUTH_WEST = (1, -1, sqrt(2))
-    NORTH_WEST = (-1, -1, sqrt(2))
+    NE = (-1, 1, np.around(np.sqrt(2), decimals=2))
+    SE = (1, 1, np.around(np.sqrt(2), decimals=2))
+    NW = (-1, -1, np.around(np.sqrt(2), decimals=2))
+    SW = (1, -1, np.around(np.sqrt(2), decimals=2))
 
     @property
     def cost(self):
@@ -89,23 +123,19 @@ def valid_actions(grid, current_node):
         valid_actions.remove(Action.WEST)
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
-    if x + 1 > n or y + 1 > m or grid[x + 1, y + 1] == 1:
-        valid_actions.remove(Action.SOUTH_EAST)
-    if x - 1 < 0 or y + 1 > m or grid[x - 1, y + 1] == 1:
-        valid_actions.remove(Action.NORTH_EAST)
-    if x + 1 > n or y - 1 < 0 or grid[x + 1, y - 1] == 1:
-        valid_actions.remove(Action.SOUTH_WEST)
-    if x - 1 < 0 or y - 1 < 0 or grid[x - 1, y - 1] == 1:
-        valid_actions.remove(Action.NORTH_WEST)
+    if x-1 < 0 or y+1 > m or grid[x-1, y+1] == 1:
+        valid_actions.remove(Action.NE)
+    if x+1 > n or y+1 > m or grid[x+1, y+1] == 1:
+        valid_actions.remove(Action.SE)
+    if x-1 < 0 or y-1 < 0 or grid[x-1, y-1] == 1:
+        valid_actions.remove(Action.NW)
+    if x+1 > n or y-1 < 0 or grid[x+1, y-1] == 1:
+        valid_actions.remove(Action.SW)
 
     return valid_actions
 
 
 def a_star(grid, h, start, goal):
-    """
-    Given a grid and heuristic function returns
-    the lowest cost path from start to goal.
-    """
 
     path = []
     path_cost = 0
@@ -118,24 +148,28 @@ def a_star(grid, h, start, goal):
 
     while not queue.empty():
         item = queue.get()
-        current_cost = item[0]
         current_node = item[1]
+        if current_node == start:
+            current_cost = 0.0
+        else:
+            current_cost = branch[current_node][0]
 
         if current_node == goal:
             print('Found a path.')
             found = True
             break
         else:
-            # Get the new vertexes connected to the current vertex
-            for a in valid_actions(grid, current_node):
-                next_node = (current_node[0] + a.delta[0], current_node[1] + a.delta[1])
-                new_cost = current_cost + a.cost + h(next_node, goal)
+            for action in valid_actions(grid, current_node):
+                # get the tuple representation
+                da = action.delta
+                next_node = (current_node[0] + da[0], current_node[1] + da[1])
+                branch_cost = current_cost + action.cost
+                queue_cost = branch_cost + h(next_node, goal)
 
                 if next_node not in visited:
                     visited.add(next_node)
-                    queue.put((new_cost, next_node))
-
-                    branch[next_node] = (new_cost, current_node, a)
+                    branch[next_node] = (branch_cost, current_node, action)
+                    queue.put((queue_cost, next_node))
 
     if found:
         # retrace steps
@@ -152,51 +186,27 @@ def a_star(grid, h, start, goal):
         print('**********************')
     return path[::-1], path_cost
 
+
 def heuristic(position, goal_position):
     return np.linalg.norm(np.array(position) - np.array(goal_position))
 
-def read_home(filename):
-    """
-    Reads home (lat, lon) from the first line of the `file`.
-    """
-    with open(filename) as f:
-        first_line = f.readline()
-    match = re.match(r'^lat0 (.*), lon0 (.*)$', first_line)
-    if match:
-        lat = match.group(1)
-        lon = match.group(2)
-    return np.fromstring(f'{lat},{lon}', dtype='Float64', sep=',')
 
-def collinearity_prune(path, epsilon=1e-5):
+def bres_prune(grid, path):
     """
-    Prune path points from `path` using collinearity.
+    Use the Bresenham module to trim uneeded waypoints from path
     """
-    def point(p):
-        return np.array([p[0], p[1], 1.]).reshape(1, -1)
-
-    def collinearity_check(p1, p2, p3):
-        m = np.concatenate((p1, p2, p3), 0)
-        det = np.linalg.det(m)
-        return abs(det) < epsilon
-
     pruned_path = [p for p in path]
+
     i = 0
     while i < len(pruned_path) - 2:
-        p1 = point(pruned_path[i])
-        p2 = point(pruned_path[i+1])
-        p3 = point(pruned_path[i+2])
+        p1 = pruned_path[i]
+        p2 = pruned_path[i+1]
+        p3 = pruned_path[i+2]
 
-        # If the 3 points are in a line remove
-        # the 2nd point.
-        # The 3rd point now becomes and 2nd point
-        # and the check is redone with a new third point
-        # on the next iteration.
-        if collinearity_check(p1, p2, p3):
-            # Something subtle here but we can mutate
-            # `pruned_path` freely because the length
-            # of the list is check on every iteration.
-            pruned_path.remove(pruned_path[i+1])
+        points = bresenham(p1[0], p1[1], p3[0], p3[1])
+        if all(grid[point[0], point[1]] == 0 for point in points):
+            pruned_path.remove(p2)
         else:
             i += 1
-    return pruned_path
 
+    return pruned_path
